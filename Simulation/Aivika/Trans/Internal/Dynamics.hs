ad170e39cf198e7ba0f541ce2acef7e1b05f0602
David Sorokin <david.sorokin@gmail.com>
2014-10-20 07:08:06 +0400

    Simplified optimisation pragmas in module Dynamics.

--- Simulation/Aivika/Trans/Internal/Dynamics.hs
+++ Simulation/Aivika/Trans/Internal/Dynamics.hs
@@ -43,10 +43,10 @@ import Simulation.Aivika.Trans.Internal.Simulation
 
 instance Monad m => Monad (Dynamics m) where
 
-  {-# SPECIALISE INLINE return :: a -> Dynamics IO a #-}
+  {-# INLINE return #-}
   return a = Dynamics $ \p -> return a
 
-  {-# SPECIALISE INLINE (>>=) :: Dynamics IO a -> (a -> Dynamics IO b) -> Dynamics IO b #-}
+  {-# INLINE (>>=) #-}
   (Dynamics m) >>= k =
     Dynamics $ \p -> 
     do a <- m p
@@ -55,58 +55,49 @@ instance Monad m => Monad (Dynamics m) where
 
 -- | Run the 'Dynamics' computation in the initial time point.
 runDynamicsInStartTime :: Dynamics m a -> Simulation m a
-{-# INLINE runDynamicsInStartTime #-}
 runDynamicsInStartTime (Dynamics m) =
   Simulation $ m . integStartPoint
 
 -- | Run the 'Dynamics' computation in the final time point.
 runDynamicsInStopTime :: Dynamics m a -> Simulation m a
-{-# INLINE runDynamicsInStopTime #-}
 runDynamicsInStopTime (Dynamics m) =
   Simulation $ m . integStopPoint
 
 -- | Run the 'Dynamics' computation in all integration time points.
 runDynamicsInIntegTimes :: Monad m => Dynamics m a -> Simulation m [m a]
-{-# INLINABLE runDynamicsInIntegTimes #-}
-{-# SPECIALISE runDynamicsInIntegTimes :: Dynamics IO a -> Simulation IO [IO a] #-}
 runDynamicsInIntegTimes (Dynamics m) =
   Simulation $ return . map m . integPoints
 
 -- | Run the 'Dynamics' computation in the specified time point.
 runDynamicsInTime :: Double -> Dynamics m a -> Simulation m a
-{-# INLINE runDynamicsInTime #-}
 runDynamicsInTime t (Dynamics m) =
   Simulation $ \r -> m $ pointAt r t
 
 -- | Run the 'Dynamics' computation in the specified time points.
 runDynamicsInTimes :: Monad m => [Double] -> Dynamics m a -> Simulation m [m a]
-{-# INLINABLE runDynamicsInTimes #-}
-{-# SPECIALISE runDynamicsInTimes :: [Double] -> Dynamics IO a -> Simulation IO [IO a] #-}
 runDynamicsInTimes ts (Dynamics m) =
   Simulation $ \r -> return $ map (m . pointAt r) ts 
 
 instance Functor m => Functor (Dynamics m) where
   
-  {-# SPECIALISE INLINE fmap :: (a -> b) -> Dynamics IO a -> Dynamics IO b #-}
+  {-# INLINE fmap #-}
   fmap f (Dynamics x) = Dynamics $ \p -> fmap f $ x p
 
 instance Applicative m => Applicative (Dynamics m) where
   
-  {-# SPECIALISE INLINE pure :: a -> Dynamics IO a #-}
+  {-# INLINE pure #-}
   pure = Dynamics . const . pure
   
-  {-# SPECIALISE INLINE (<*>) :: Dynamics IO (a -> b) -> Dynamics IO a -> Dynamics IO b #-}
+  {-# INLINE (<*>) #-}
   (Dynamics x) <*> (Dynamics y) = Dynamics $ \p -> x p <*> y p
 
 liftMD :: Monad m => (a -> b) -> Dynamics m a -> Dynamics m b
-{-# INLINABLE liftMD #-}
-{-# SPECIALISE liftMD :: (a -> b) -> Dynamics IO a -> Dynamics IO b #-}
+{-# INLINE liftMD #-}
 liftMD f (Dynamics x) =
   Dynamics $ \p -> do { a <- x p; return $ f a }
 
 liftM2D :: Monad m => (a -> b -> c) -> Dynamics m a -> Dynamics m b -> Dynamics m c
-{-# INLINABLE liftM2D #-}
-{-# SPECIALISE liftM2D :: (a -> b -> c) -> Dynamics IO a -> Dynamics IO b -> Dynamics IO c #-}
+{-# INLINE liftM2D #-}
 liftM2D f (Dynamics x) (Dynamics y) =
   Dynamics $ \p -> do { a <- x p; b <- y p; return $ f a b }
 
@@ -235,8 +226,6 @@ instance ParameterLift Dynamics where
   
 -- | Exception handling within 'Dynamics' computations.
 catchDynamics :: (Comp m, Exception e) => Dynamics m a -> (e -> Dynamics m a) -> Dynamics m a
-{-# INLINABLE catchDynamics #-}
-{-# SPECIALISE catchDynamics :: Exception e => Dynamics IO a -> (e -> Dynamics IO a) -> Dynamics IO a #-}
 catchDynamics (Dynamics m) h =
   Dynamics $ \p -> 
   catchComp (m p) $ \e ->
@@ -244,46 +233,38 @@ catchDynamics (Dynamics m) h =
                            
 -- | A computation with finalization part like the 'finally' function.
 finallyDynamics :: Comp m => Dynamics m a -> Dynamics m b -> Dynamics m a
-{-# INLINABLE finallyDynamics #-}
-{-# SPECIALISE finallyDynamics :: Dynamics IO a -> Dynamics IO b -> Dynamics IO a #-}
 finallyDynamics (Dynamics m) (Dynamics m') =
   Dynamics $ \p ->
   finallyComp (m p) (m' p)
 
 -- | Like the standard 'throw' function.
 throwDynamics :: (Comp m, Exception e) => e -> Dynamics m a
-{-# INLINABLE throwDynamics #-}
-{-# SPECIALISE throwDynamics :: Exception e => e -> Dynamics IO a #-}
 throwDynamics = throw
 
 instance MonadFix m => MonadFix (Dynamics m) where
 
-  {-# SPECIALISE INLINE mfix :: (a -> Dynamics IO a) -> Dynamics IO a #-}
+  {-# INLINE mfix #-}
   mfix f = 
     Dynamics $ \p ->
     do { rec { a <- invokeDynamics p (f a) }; return a }
 
 -- | Computation that returns the current simulation time.
 time :: Monad m => Dynamics m Double
-{-# INLINABLE time #-}
-{-# SPECIALISE time :: Dynamics IO Double #-}
+{-# INLINE time #-}
 time = Dynamics $ return . pointTime 
 
 -- | Whether the current time is an integration time.
 isTimeInteg :: Monad m => Dynamics m Bool
-{-# INLINABLE isTimeInteg #-}
-{-# SPECIALISE isTimeInteg :: Dynamics IO Bool #-}
+{-# INLINE isTimeInteg #-}
 isTimeInteg = Dynamics $ \p -> return $ pointPhase p >= 0
 
 -- | Return the integration iteration closest to the current simulation time.
 integIteration :: Monad m => Dynamics m Int
-{-# INLINABLE integIteration #-}
-{-# SPECIALISE integIteration :: Dynamics IO Int #-}
+{-# INLINE integIteration #-}
 integIteration = Dynamics $ return . pointIteration
 
 -- | Return the integration phase for the current simulation time.
 -- It is @(-1)@ for non-integration time points.
 integPhase :: Monad m => Dynamics m Int
-{-# INLINABLE integPhase #-}
-{-# SPECIALISE integPhase :: Dynamics IO Int #-}
+{-# INLINE integPhase #-}
 integPhase = Dynamics $ return . pointPhase
