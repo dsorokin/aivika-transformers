52f269b8c0d05bebea09529de964474e206d5a29
David Sorokin <david.sorokin@gmail.com>
2016-02-05 16:56:43 +0300

    Updated module Dynamics.Random.

--- Simulation/Aivika/Trans/Dynamics/Random.hs
+++ Simulation/Aivika/Trans/Dynamics/Random.hs
@@ -22,11 +22,17 @@
 module Simulation.Aivika.Trans.Dynamics.Random
        (memoRandomUniformDynamics,
         memoRandomUniformIntDynamics,
+        memoRandomTriangularDynamics,
         memoRandomNormalDynamics,
+        memoRandomLogNormalDynamics,
         memoRandomExponentialDynamics,
         memoRandomErlangDynamics,
         memoRandomPoissonDynamics,
-        memoRandomBinomialDynamics) where
+        memoRandomBinomialDynamics,
+        memoRandomGammaDynamics,
+        memoRandomBetaDynamics,
+        memoRandomWeibullDynamics,
+        memoRandomDiscreteDynamics) where
 
 import Simulation.Aivika.Trans.Generator
 import Simulation.Aivika.Trans.Internal.Specs
@@ -66,6 +72,23 @@ memoRandomUniformIntDynamics min max =
      max' <- invokeDynamics p max
      generateUniformInt g min' max'
 
+-- | Computation that generates random numbers from the triangular distribution
+-- and memoizes the numbers in the integration time points.
+memoRandomTriangularDynamics :: MonadSD m
+                                => Dynamics m Double  -- ^ minimum
+                                -> Dynamics m Double  -- ^ median
+                                -> Dynamics m Double  -- ^ maximum
+                                -> Simulation m (Dynamics m Double)
+{-# INLINABLE memoRandomTriangularDynamics #-}
+memoRandomTriangularDynamics min median max =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     min' <- invokeDynamics p min
+     median' <- invokeDynamics p median
+     max' <- invokeDynamics p max
+     generateTriangular g min' median' max'
+
 -- | Computation that generates random numbers distributed normally and
 -- memoizes them in the integration time points.
 memoRandomNormalDynamics :: MonadSD m
@@ -80,6 +103,25 @@ memoRandomNormalDynamics mu nu =
      mu' <- invokeDynamics p mu
      nu' <- invokeDynamics p nu
      generateNormal g mu' nu'
+
+-- | Computation that generates random numbers from the lognormal distribution
+-- and memoizes the numbers in the integration time points.
+memoRandomLogNormalDynamics :: MonadSD m
+                               => Dynamics m Double
+                               -- ^ the mean of a normal distribution which
+                               -- this distribution is derived from
+                               -> Dynamics m Double
+                               -- ^ the deviation of a normal distribution which
+                               -- this distribution is derived from
+                               -> Simulation m (Dynamics m Double)
+{-# INLINABLE memoRandomLogNormalDynamics #-}
+memoRandomLogNormalDynamics mu nu =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     mu' <- invokeDynamics p mu
+     nu' <- invokeDynamics p nu
+     generateLogNormal g mu' nu'
 
 -- | Computation that generates exponential random numbers with the specified mean
 -- (the reciprocal of the rate) and memoizes them in the integration time points.
@@ -141,3 +183,62 @@ memoRandomBinomialDynamics prob trials =
      prob' <- invokeDynamics p prob
      trials' <- invokeDynamics p trials
      generateBinomial g prob' trials'
+
+-- | Computation that generates random numbers from the Gamma distribution
+-- with the specified shape and scale but memoizes the numbers in
+-- the integration time points.
+memoRandomGammaDynamics :: MonadSD m
+                           => Dynamics m Double  -- ^ shape
+                           -> Dynamics m Double  -- ^ scale (a reciprocal of the rate)
+                           -> Simulation m (Dynamics m Double)
+{-# INLINABLE memoRandomGammaDynamics #-}
+memoRandomGammaDynamics kappa theta =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     kappa' <- invokeDynamics p kappa
+     theta' <- invokeDynamics p theta
+     generateGamma g kappa' theta'
+
+-- | Computation that generates random numbers from the Beta distribution
+-- by the specified shape parameters and memoizes the numbers in
+-- the integration time points.
+memoRandomBetaDynamics :: MonadSD m
+                          => Dynamics m Double  -- ^ shape (alpha)
+                          -> Dynamics m Double  -- ^ shape (beta)
+                          -> Simulation m (Dynamics m Double)
+{-# INLINABLE memoRandomBetaDynamics #-}
+memoRandomBetaDynamics alpha beta =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     alpha' <- invokeDynamics p alpha
+     beta'  <- invokeDynamics p beta
+     generateBeta g alpha' beta'
+
+-- | Computation that generates random numbers from the Weibull distribution
+-- with the specified shape and scale but memoizes the numbers in
+-- the integration time points.
+memoRandomWeibullDynamics :: MonadSD m
+                             => Dynamics m Double  -- ^ shape
+                             -> Dynamics m Double  -- ^ scale
+                             -> Simulation m (Dynamics m Double)
+{-# INLINABLE memoRandomWeibullDynamics #-}
+memoRandomWeibullDynamics alpha beta =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     alpha' <- invokeDynamics p alpha
+     beta'  <- invokeDynamics p beta
+     generateWeibull g alpha' beta'
+
+-- | Computation that generates random values from the specified discrete
+-- distribution and memoizes the values in the integration time points.
+memoRandomDiscreteDynamics :: (MonadSD m, MonadMemo m a) => Dynamics m (DiscretePDF a) -> Simulation m (Dynamics m a)
+{-# INLINABLE memoRandomDiscreteDynamics #-}
+memoRandomDiscreteDynamics dpdf =
+  memo0Dynamics $
+  Dynamics $ \p ->
+  do let g = runGenerator $ pointRun p
+     dpdf' <- invokeDynamics p dpdf
+     generateDiscrete g dpdf'
