fbe1ef8303ff4016e7db238f0e7d21e8294857e5
David Sorokin <david.sorokin@gmail.com>
2016-02-05 18:22:37 +0300

    Updated module Processor.Random.

--- Simulation/Aivika/Trans/Processor/Random.hs
+++ Simulation/Aivika/Trans/Processor/Random.hs
@@ -15,13 +15,20 @@
 module Simulation.Aivika.Trans.Processor.Random
        (randomUniformProcessor,
         randomUniformIntProcessor,
+        randomTriangularProcessor,
         randomNormalProcessor,
+        randomLogNormalProcessor,
         randomExponentialProcessor,
         randomErlangProcessor,
         randomPoissonProcessor,
-        randomBinomialProcessor) where
+        randomBinomialProcessor,
+        randomGammaProcessor,
+        randomBetaProcessor,
+        randomWeibullProcessor,
+        randomDiscreteProcessor) where
 
 import Simulation.Aivika.Trans.DES
+import Simulation.Aivika.Trans.Generator
 import Simulation.Aivika.Trans.Process
 import Simulation.Aivika.Trans.Process.Random
 import Simulation.Aivika.Trans.Processor
@@ -53,6 +60,21 @@ randomUniformIntProcessor min max =
   randomUniformIntProcess_ min max
 
 -- | When processing every input element, hold the process
+-- for a random time interval having the triangular distribution.
+randomTriangularProcessor :: MonadDES m
+                             => Double
+                             -- ^ the minimum time interval
+                             -> Double
+                             -- ^ the median of the time interval
+                             -> Double
+                             -- ^ the maximum time interval
+                             -> Processor m a a
+{-# INLINABLE randomTriangularProcessor #-}
+randomTriangularProcessor min median max =
+  withinProcessor $
+  randomTriangularProcess_ min median max
+
+-- | When processing every input element, hold the process
 -- for a random time interval distributed normally.
 randomNormalProcessor :: MonadDES m
                          => Double
@@ -64,6 +86,21 @@ randomNormalProcessor :: MonadDES m
 randomNormalProcessor mu nu =
   withinProcessor $
   randomNormalProcess_ mu nu
+         
+-- | When processing every input element, hold the process
+-- for a random time interval having the lognormal distribution.
+randomLogNormalProcessor :: MonadDES m
+                            => Double
+                            -- ^ the mean for a normal distribution
+                            -- which this distribution is derived from
+                            -> Double
+                            -- ^ the deviation for a normal distribution
+                            -- which this distribution is derived from
+                            -> Processor m a a
+{-# INLINABLE randomLogNormalProcessor #-}
+randomLogNormalProcessor mu nu =
+  withinProcessor $
+  randomLogNormalProcess_ mu nu
          
 -- | When processing every input element, hold the process
 -- for a random time interval distributed exponentially
@@ -116,3 +153,56 @@ randomBinomialProcessor :: MonadDES m
 randomBinomialProcessor prob trials =
   withinProcessor $
   randomBinomialProcess_ prob trials
+
+-- | When processing every input element, hold the process
+-- for a random time interval having the Gamma distribution
+-- with the specified shape and scale.
+randomGammaProcessor :: MonadDES m
+                        => Double
+                        -- ^ the shape
+                        -> Double
+                        -- ^ the scale (a reciprocal of the rate)
+                        -> Processor m a a
+{-# INLINABLE randomGammaProcessor #-}
+randomGammaProcessor kappa theta =
+  withinProcessor $
+  randomGammaProcess_ kappa theta
+
+-- | When processing every input element, hold the process
+-- for a random time interval having the Beta distribution
+-- with the specified shape parameters (alpha and beta).
+randomBetaProcessor :: MonadDES m
+                       => Double
+                       -- ^ shape (alpha)
+                       -> Double
+                       -- ^ shape (beta)
+                       -> Processor m a a
+{-# INLINABLE randomBetaProcessor #-}
+randomBetaProcessor alpha beta =
+  withinProcessor $
+  randomBetaProcess_ alpha beta
+
+-- | When processing every input element, hold the process
+-- for a random time interval having the Weibull distribution
+-- with the specified shape and scale.
+randomWeibullProcessor :: MonadDES m
+                          => Double
+                          -- ^ shape
+                          -> Double
+                          -- ^ scale
+                          -> Processor m a a
+{-# INLINABLE randomWeibullProcessor #-}
+randomWeibullProcessor alpha beta =
+  withinProcessor $
+  randomWeibullProcess_ alpha beta
+
+-- | When processing every input element, hold the process
+-- for a random time interval having the specified discrete distribution.
+randomDiscreteProcessor :: MonadDES m
+                           => DiscretePDF Double
+                           -- ^ the discrete probability density function
+                           -> Processor m a a
+{-# INLINABLE randomDiscreteProcessor #-}
+randomDiscreteProcessor dpdf =
+  withinProcessor $
+  randomDiscreteProcess_ dpdf
