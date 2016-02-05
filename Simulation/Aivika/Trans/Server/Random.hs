6b0c15967491ceff784830588da3d9c526c46d4c
David Sorokin <david.sorokin@gmail.com>
2016-02-05 18:44:21 +0300

    Updated module Server.Random.

--- Simulation/Aivika/Trans/Server/Random.hs
+++ Simulation/Aivika/Trans/Server/Random.hs
@@ -15,20 +15,33 @@
 module Simulation.Aivika.Trans.Server.Random
        (newRandomUniformServer,
         newRandomUniformIntServer,
+        newRandomTriangularServer,
         newRandomNormalServer,
+        newRandomLogNormalServer,
         newRandomExponentialServer,
         newRandomErlangServer,
         newRandomPoissonServer,
         newRandomBinomialServer,
+        newRandomGammaServer,
+        newRandomBetaServer,
+        newRandomWeibullServer,
+        newRandomDiscreteServer,
         newPreemptibleRandomUniformServer,
         newPreemptibleRandomUniformIntServer,
+        newPreemptibleRandomTriangularServer,
         newPreemptibleRandomNormalServer,
+        newPreemptibleRandomLogNormalServer,
         newPreemptibleRandomExponentialServer,
         newPreemptibleRandomErlangServer,
         newPreemptibleRandomPoissonServer,
-        newPreemptibleRandomBinomialServer) where
+        newPreemptibleRandomBinomialServer,
+        newPreemptibleRandomGammaServer,
+        newPreemptibleRandomBetaServer,
+        newPreemptibleRandomWeibullServer,
+        newPreemptibleRandomDiscreteServer) where
 
 import Simulation.Aivika.Trans.DES
+import Simulation.Aivika.Trans.Generator
 import Simulation.Aivika.Trans.Simulation
 import Simulation.Aivika.Trans.Process
 import Simulation.Aivika.Trans.Process.Random
@@ -67,6 +80,24 @@ newRandomUniformIntServer =
   newPreemptibleRandomUniformIntServer False
 
 -- | Create a new server that holds the process for a random time interval
+-- having the triangular distribution, when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomTriangularServer :: MonadDES m
+                             => Double
+                             -- ^ the minimum time interval
+                             -> Double
+                             -- ^ the median of the time interval
+                             -> Double
+                             -- ^ the maximum time interval
+                             -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomTriangularServer #-}
+newRandomTriangularServer =
+  newPreemptibleRandomTriangularServer False
+
+-- | Create a new server that holds the process for a random time interval
 -- distributed normally, when processing every input element.
 --
 -- By default, it is assumed that the server process cannot be preempted,
@@ -81,6 +112,24 @@ newRandomNormalServer :: MonadDES m
 {-# INLINABLE newRandomNormalServer #-}
 newRandomNormalServer =
   newPreemptibleRandomNormalServer False
+         
+-- | Create a new server that holds the process for a random time interval
+-- having the lognormal distribution, when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomLogNormalServer :: MonadDES m
+                            => Double
+                            -- ^ the mean of a normal distribution which
+                            -- this distribution is derived from
+                            -> Double
+                            -- ^ the deviation of a normal distribution which
+                            -- this distribution is derived from
+                            -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomLogNormalServer #-}
+newRandomLogNormalServer =
+  newPreemptibleRandomLogNormalServer False
          
 -- | Create a new server that holds the process for a random time interval
 -- distributed exponentially with the specified mean (the reciprocal of the rate),
@@ -147,6 +196,71 @@ newRandomBinomialServer =
   newPreemptibleRandomBinomialServer False
 
 -- | Create a new server that holds the process for a random time interval
+-- having the Gamma distribution with the specified shape and scale,
+-- when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomGammaServer :: MonadDES m
+                        => Double
+                        -- ^ the shape
+                        -> Double
+                        -- ^ the scale (a reciprocal of the rate)
+                        -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomGammaServer #-}
+newRandomGammaServer =
+  newPreemptibleRandomGammaServer False
+
+-- | Create a new server that holds the process for a random time interval
+-- having the Beta distribution with the specified shape parameters (alpha and beta),
+-- when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomBetaServer :: MonadDES m
+                       => Double
+                       -- ^ shape (alpha)
+                       -> Double
+                       -- ^ shape (beta)
+                       -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomBetaServer #-}
+newRandomBetaServer =
+  newPreemptibleRandomBetaServer False
+
+-- | Create a new server that holds the process for a random time interval
+-- having the Weibull distribution with the specified shape and scale,
+-- when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomWeibullServer :: MonadDES m
+                          => Double
+                          -- ^ shape
+                          -> Double
+                          -- ^ scale
+                          -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomWeibullServer #-}
+newRandomWeibullServer =
+  newPreemptibleRandomWeibullServer False
+
+-- | Create a new server that holds the process for a random time interval
+-- having the specified discrete distribution, when processing every input element.
+--
+-- By default, it is assumed that the server process cannot be preempted,
+-- because the handling of possible task preemption is rather costly
+-- operation.
+newRandomDiscreteServer :: MonadDES m
+                           => DiscretePDF Double
+                           -- ^ the discrete probability density function
+                           -> Simulation m (Server m () a a)
+{-# INLINABLE newRandomDiscreteServer #-}
+newRandomDiscreteServer =
+  newPreemptibleRandomDiscreteServer False
+
+-- | Create a new server that holds the process for a random time interval
 -- distributed uniformly, when processing every input element.
 newPreemptibleRandomUniformServer :: MonadDES m
                                      => Bool
@@ -179,6 +293,24 @@ newPreemptibleRandomUniformIntServer preemptible min max =
      return a
 
 -- | Create a new server that holds the process for a random time interval
+-- having the triangular distribution, when processing every input element.
+newPreemptibleRandomTriangularServer :: MonadDES m
+                                        => Bool
+                                        -- ^ whether the server process can be preempted
+                                        -> Double
+                                        -- ^ the minimum time interval
+                                        -> Double
+                                        -- ^ the median of the time interval
+                                        -> Double
+                                        -- ^ the maximum time interval
+                                        -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomTriangularServer #-}
+newPreemptibleRandomTriangularServer preemptible min median max =
+  newPreemptibleServer preemptible $ \a ->
+  do randomTriangularProcess_ min median max
+     return a
+
+-- | Create a new server that holds the process for a random time interval
 -- distributed normally, when processing every input element.
 newPreemptibleRandomNormalServer :: MonadDES m
                                     => Bool
@@ -192,6 +324,24 @@ newPreemptibleRandomNormalServer :: MonadDES m
 newPreemptibleRandomNormalServer preemptible mu nu =
   newPreemptibleServer preemptible $ \a ->
   do randomNormalProcess_ mu nu
+     return a
+
+-- | Create a new server that holds the process for a random time interval
+-- having the lognormal distribution, when processing every input element.
+newPreemptibleRandomLogNormalServer :: MonadDES m
+                                       => Bool
+                                       -- ^ whether the server process can be preempted
+                                       -> Double
+                                       -- ^ the mean of a normal distribution which
+                                       -- this distribution is derived from
+                                       -> Double
+                                       -- ^ the deviation of a normal distribution which
+                                       -- this distribution is derived from
+                                       -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomLogNormalServer #-}
+newPreemptibleRandomLogNormalServer preemptible mu nu =
+  newPreemptibleServer preemptible $ \a ->
+  do randomLogNormalProcess_ mu nu
      return a
          
 -- | Create a new server that holds the process for a random time interval
@@ -256,4 +406,69 @@ newPreemptibleRandomBinomialServer :: MonadDES m
 newPreemptibleRandomBinomialServer preemptible prob trials =
   newPreemptibleServer preemptible $ \a ->
   do randomBinomialProcess_ prob trials
+     return a
+
+-- | Create a new server that holds the process for a random time interval
+-- having the Gamma distribution with the specified shape and scale,
+-- when processing every input element.
+newPreemptibleRandomGammaServer :: MonadDES m
+                                   => Bool
+                                   -- ^ whether the server process can be preempted
+                                   -> Double
+                                   -- ^ the shape
+                                   -> Double
+                                   -- ^ the scale (a reciprocal of the rate)
+                                   -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomGammaServer #-}
+newPreemptibleRandomGammaServer preemptible kappa theta =
+  newPreemptibleServer preemptible $ \a ->
+  do randomGammaProcess_ kappa theta
+     return a
+
+-- | Create a new server that holds the process for a random time interval
+-- having the Beta distribution with the specified shape parameters (alpha and beta),
+-- when processing every input element.
+newPreemptibleRandomBetaServer :: MonadDES m
+                                  => Bool
+                                  -- ^ whether the server process can be preempted
+                                  -> Double
+                                  -- ^ shape (alpha)
+                                  -> Double
+                                  -- ^ shape (beta)
+                                  -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomBetaServer #-}
+newPreemptibleRandomBetaServer preemptible alpha beta =
+  newPreemptibleServer preemptible $ \a ->
+  do randomBetaProcess_ alpha beta
+     return a
+
+-- | Create a new server that holds the process for a random time interval
+-- having the Weibull distribution with the specified shape and scale,
+-- when processing every input element.
+newPreemptibleRandomWeibullServer :: MonadDES m
+                                     => Bool
+                                     -- ^ whether the server process can be preempted
+                                     -> Double
+                                     -- ^ shape
+                                     -> Double
+                                     -- ^ scale
+                                     -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomWeibullServer #-}
+newPreemptibleRandomWeibullServer preemptible alpha beta =
+  newPreemptibleServer preemptible $ \a ->
+  do randomWeibullProcess_ alpha beta
+     return a
+
+-- | Create a new server that holds the process for a random time interval
+-- having the specified discrete distribution, when processing every input element.
+newPreemptibleRandomDiscreteServer :: MonadDES m
+                                      => Bool
+                                      -- ^ whether the server process can be preempted
+                                      -> DiscretePDF Double
+                                      -- ^ the discrete probability density function
+                                      -> Simulation m (Server m () a a)
+{-# INLINABLE newPreemptibleRandomDiscreteServer #-}
+newPreemptibleRandomDiscreteServer preemptible dpdf =
+  newPreemptibleServer preemptible $ \a ->
+  do randomDiscreteProcess_ dpdf
      return a
