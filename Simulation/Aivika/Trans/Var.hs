b34bc715f152b41c2f78ab3d3ac910a8cb70efd3
David Sorokin <david.sorokin@gmail.com>
2017-09-13 09:12:51 +0300

    Updated comments

--- Simulation/Aivika/Trans/Var.hs
+++ Simulation/Aivika/Trans/Var.hs
@@ -14,6 +14,12 @@
 -- allows using the variable in differential and difference equations of
 -- System Dynamics within hybrid discrete-continuous simulation.
 --
+-- Because of using the arrays, it would usually be a logical mistake to
+-- use this variable for collecting statistics. In most cases,
+-- the statistics can actually be collected with a very small footprint
+-- by updating immutable 'SamplingStats' and 'TimingStats' values in
+-- a mutable 'Ref' reference.
+--
 module Simulation.Aivika.Trans.Var (MonadVar(..)) where
 
 import Data.Array
@@ -24,19 +30,24 @@ import Simulation.Aivika.Trans.Internal.Simulation
 import Simulation.Aivika.Trans.Internal.Dynamics
 import Simulation.Aivika.Trans.Internal.Event
 import Simulation.Aivika.Trans.Signal
+import Simulation.Aivika.Trans.Statistics
 
 -- | A type class of monads within which we can create mutable variables.
 class MonadDES m => MonadVar m where
 
   -- | Like the 'Ref' reference but keeps the history of changes in 
   -- different time points. The 'Var' variable is safe to be used in
-  -- the hybrid discrete-continuous simulation.
+  -- the hybrid discrete-continuous simulation. Only this variable can
+  -- be much slower than the reference.
   --
-  -- For example, the memoised values of a variable can be used in
-  -- the differential or difference equations of System Dynamics, while
-  -- the variable iself can be updated wihin the discrete event simulation.
+  -- For example, the memoised values of the variable can be used in
+  -- the differential and difference equations of System Dynamics, while
+  -- the variable iself can be updated within the discrete event simulation.
   --
-  -- Only this variable is much slower than the reference.
+  -- Because of using arrays, it would usually be a logical mistake to use
+  -- the variable for collecting statistics. In most cases, the statistics
+  -- can actually be collected with a very small footprint by updating immutable
+  -- 'SamplingStats' and 'TimingStats' values in a mutable @Ref@ reference.
   data Var m a
      
   -- | Create a new variable.
@@ -51,7 +62,7 @@ class MonadDES m => MonadVar m where
   
   -- | Read the recent actual value of a variable for the requested time.
   --
-  -- This computation is destined for using within discrete event simulation.
+  -- This computation is destined to be used within discrete event simulation.
   readVar :: Var m a -> Event m a
   
   -- | Write a new value into the variable.
@@ -60,7 +71,7 @@ class MonadDES m => MonadVar m where
   -- | Mutate the contents of the variable.
   modifyVar :: Var m a -> (a -> a) -> Event m ()
 
-  -- | Freeze the variable and return in arrays the time points and corresponded 
+  -- | Freeze the variable and return in arrays the time points and the corresponding 
   -- first and last values when the variable had changed or had been memoised in
   -- different time points: (1) the time points are sorted in ascending order;
   -- (2) the first and last actual values per each time point are provided.
